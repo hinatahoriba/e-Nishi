2e83b88509494022582fba3e1c1afbe9726feb96
hinatahoriba <c0a2212903@edu.teu.ac.jp>
2026-06-19 14:14:02 +0900

    add: GTM導入した

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import {
   Montserrat,
   Noto_Sans_JP,
 } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const notoSansJP = Noto_Sans_JP({
@@ -31,6 +32,8 @@ const dmSans = DM_Sans({
   weight: ["300", "400"],
 });
 
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+
 export const metadata = {
   title: "E-NISHI",
   description: "デジタル名刺を作成・共有できるWebアプリ",
@@ -47,8 +50,33 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         />
+        {GTM_ID ? (
+          <Script id="gtm" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `}
+          </Script>
+        ) : null}
       </head>
       <body className="min-h-full flex flex-col bg-[#FBFBFB] text-[#222]">
+        {GTM_ID ? (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        ) : null}
         {children}
       </body>
     </html>
